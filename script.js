5f622830a851919d7aabe883d0430cdece869b4a
Soumyadip Roy <85121861+Soumyadip-Roy@users.noreply.github.com>
2022-08-20 22:23:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,12 +16,20 @@ onGenerateSubmit = (e)=>{
     else{
         console.log(url,size);
         showSpinner();
+
         setTimeout(() => {
             hideSpinner();
+
             generateQRCode(url,size);
+
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+                console.log(saveUrl);
+                createSaveBtn(saveUrl);
+            }, 50);
         }, 1000);
 
-
+        
     }
 
 }
@@ -42,10 +50,22 @@ const hideSpinner = () =>{
 }
 
 const clearui = ()=>{
-    qr.innerHTML=''
+    qr.innerHTML='';
+    const saveLink = document.getElementById('save-link');
+    if(saveLink){
+        saveLink.remove();
+    }
+
 }
+
 const createSaveBtn = (saveUrl)=>{
-    
+    const link = document.createElement('a');
+    link.id='save-link';
+    link.classList = `bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 px-20 rounded m-auto my-5`;
+    link.href=saveUrl;
+    link.download='qrcode';
+    link.innerHTML="Save Image";
+    document.getElementById('generated').appendChild(link);
 }
 form.addEventListener('submit',onGenerateSubmit);
 
